7a7840cc473e2f33f280929e8998666b0c19e05c
thirud4a4 <122852236+thirud4a4@users.noreply.github.com>
2026-01-13 18:22:10 +0530

    Update author name in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/thirud4a4/TeluguMovies")
-authors = listOf("Durgam")
+authors = listOf("thirud4a4")
     }
 
     android {
